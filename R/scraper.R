e20f15c8f61b7dadb474a38daa5a78f0192b0bbf
Alex Truong <haiyen@student.ubc.ca>
2021-03-04 15:45:19 -0800

    Update scraper documentation

--- R/scraper.R
+++ R/scraper.R
@@ -2,13 +2,12 @@
 #'
 #' @param url The given craiglist url to scrape the data from
 #' @param online Whether the data is scraped directly online from the url (default = False). False means the data is scraped from local HTML file
-#' @param save_csv Whether to export the data to CSV file (default = False)
 #'
-#' @return tibble A dataframe containing listing information such as price, house type and neighborhood. A csv containing the dataframe could also be exported with `save_csv` option is activated to True.
+#' @return tibble A dataframe containing listing information such as price, house type and neighborhood.
 #' @export
 #'
 #' @examples
-#' scraper(url = 'https://vancouver.craigslist.org/search/van/apa', save_csv = True)
-scraper <- function(url, online = False, save_csv = False) {
+#' scraper(url = 'https://vancouver.craigslist.org/search/van/apa')
+scraper <- function(url, online = False) {
 
 }
