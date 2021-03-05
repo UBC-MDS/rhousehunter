a9ef4041ad1d95350f8e43aaf4eadca77b26bf66
Alex Truong <56498756+athy9193@users.noreply.github.com>
2021-03-05 09:26:39 +0800

    Update R/scraper.R
    Co-authored-by: elabandari <32809016+elabandari@users.noreply.github.com>

--- R/scraper.R
+++ R/scraper.R
@@ -1,6 +1,6 @@
 #' Function to scrape housing data from a given craiglist url
 #'
-#' @param url The given craiglist url to scrape the data from
+#' @param url The given Craiglist url to scrape the data from.
 #' @param online Whether the data is scraped directly from the url online (default = False). False means the data is scraped from a local HTML file.
 #'
 #' @return tibble A dataframe containing listing information such as price, house type and neighborhood.
