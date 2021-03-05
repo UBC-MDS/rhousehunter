c3a23d7ba110808dd3bed7b17bb2c41f4324b1c1
Ela Bandari <elabandari@gmail.com>
2021-03-05 11:53:30 -0800

    Add documentation for emailer function

--- R/send_email.R
+++ R/send_email.R
@@ -1,3 +1,17 @@
-send_email <- function(x) {
+#' A function to email search results.
+#'
+#' @param email_recipient A string containing the email address of the recipient.
+#' 
+#' @param email_subject A string containing the desired subject of the email. 
+#' Optional argument with default value of  'Results from RHouseHunter'.
+#'
+#' @return None.
+#' @export
+#'
+#' @examples 
+#' send_email("helloworld@gmail.com")
+#' send_email("helloworld@gmail.com", "Scraping Results 2")
+send_email <- function(email_recipient,
+                       email_subject = 'Results from RHouseHunter') {
 
 }
