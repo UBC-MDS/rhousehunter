c577dacf321b3efef4ad00c119d00d478128b885
JuntingHe <junting.heee@gmail.com>
2021-03-05 01:17:07 -0800

    Add documentation for cleaner function

--- R/data_cleaner.R
+++ R/data_cleaner.R
@@ -1,3 +1,14 @@
-data_cleaner <- function(x) {
+#' Clean web-scraped data.
+#'
+#' A function to clean raw web-scraped data given the path of its CSV file.
+#'
+#'
+#' @param raw_data_filepath The relative path of the raw data CSV file
+#'
+#' @return data.frame A cleaned data.frame ready for filtering and analysis.
+#' @export
+#' @examples
+#' data_cleaner("raw.csv")
+data_cleaner <- function(raw_data_filepath) {
 
 }
