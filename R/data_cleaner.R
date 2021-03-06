2fb9ce25595fde6aaa24fb71cf59a9e93cb9c370
JuntingHe <junting.heee@gmail.com>
2021-03-05 22:33:48 -0800

    Modify the documentation of the cleaner function

--- R/data_cleaner.R
+++ R/data_cleaner.R
@@ -1,13 +1,14 @@
 #' Clean web-scraped data.
 #'
-#' A function to clean web-scraped data given the raw data.frame.
+#' A function to clean web-scraped data given the tibble containing raw data .
 #'
 #'
-#' @param scraped_df A data.frame containing web-scraped data like listing url, price and house type.
+#' @param scraped_df A tibble containing web-scraped data like listing url, price and house type.
 #'
-#' @return data.frame A cleaned dataframe containing ininformation like listing url, price, number of bedroom, area in sqft, and city.
+#' @return data.frame A cleaned tibble containing information like listing url, price, number of bedrooms, area in sqft, and city.
 #' @export
 #' @examples
 #' data_cleaner(pd.read_csv('tests/cleaned_toy_data.csv'))
 data_cleaner <- function(scraped_df) {
+
 }
