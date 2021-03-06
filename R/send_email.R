15059794e7c1226b05e5ee4580da7ae9ab528599
Ela Bandari <elabandari@gmail.com>
2021-03-05 21:04:54 -0800

    Update emailer function

--- R/send_email.R
+++ R/send_email.R
@@ -2,6 +2,8 @@
 #'
 #' @param email_recipient A string containing the email address of the recipient.
 #' 
+#' @param filtered_data A tibble containing the filtered search results.
+#' 
 #' @param email_subject A string containing the desired subject of the email. 
 #' Optional argument with default value of  'Results from RHouseHunter'.
 #'
@@ -9,9 +11,9 @@
 #' @export
 #'
 #' @examples 
-#' send_email("helloworld@gmail.com")
-#' send_email("helloworld@gmail.com", "Scraping Results 2")
-send_email <- function(email_recipient,
+#' send_email("helloworld@gmail.com", df)
+#' send_email("helloworld@gmail.com", df, "Scraping Results 2")
+send_email <- function(email_recipient, filtered_data
                        email_subject = 'Results from RHouseHunter') {
 
 }
