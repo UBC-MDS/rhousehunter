f6dd7ce54d0999eff6dc86c147079a9326e28f0e
Alex Truong <haiyen@student.ubc.ca>
2021-03-11 00:47:46 -0800

    Update the body of scraper and add unit test for inputs

--- R/scraper.R
+++ R/scraper.R
@@ -3,11 +3,48 @@
 #' @param url The given Craiglist url to scrape the data from.
 #' @param online Whether the data is scraped directly from the url online (default = False). False means the data is scraped from a local HTML file.
 #'
-#' @return tibble A tibble containing listing information such as price, house type and neighborhood.
+#' @return tibble A tibble containing listing information such as listing url, price, house type and .
 #' @export
 #'
 #' @examples
-#' scraper(url = 'https://vancouver.craigslist.org/search/van/apa')
+#' scraper(url = "https://vancouver.craigslist.org/d/apartments-housing-for-rent/search/apa")
 scraper <- function(url, online = False) {
+  # PART1: Create node object from URL
+  page <- xml2::read_html(url)
 
+  # PART2: Extracted from node object
+  nodes <- rvest::html_nodes(page, ".result-info")
+  data <- tibble::tibble(
+    listing_url = character(),
+    price = character(),
+    house_type = character()
+  )
+
+  for (i in 1:length(nodes)) {
+    listing_url <- rvest::html_attr(rvest::html_node(nodes[i], "a"), "href")
+    price <- rvest::html_text(rvest::html_node(nodes[i], 'span[class="result-price"]'))
+    house_type <- gsub("\r\n", "", gsub(" ", "", rvest::html_text(rvest::html_node(nodes[i], 'span[class="housing"]')), fixed = TRUE), fixed = TRUE)
+    data <- tibble::add_row(data, listing_url = listing_url, price = price, house_type = house_type)
+  }
+
+  data
 }
+
+
+# Check out result
+url <- "https://vancouver.craigslist.org/d/apartments-housing-for-rent/search/apa"
+local_url <- "R/temp/van_housing_listings.html"
+data <- scraper(local_url)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/testthat/test-scraper.R
+++ tests/testthat/test-scraper.R
@@ -1,3 +1,47 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+url <-  'https://vancouver.craigslist.org/d/apartments-housing-for-rent/search/apa'
+
+# Tests on input
+
+# Test scraper() to raise error if URL input is missing
+test_that("ERROR when running scraper(): URL input is missing", {
+  expect_error(scraper(online = TRUE))
+  expect_error(scraper())
 })
+
+# Test scraper() to raise error if URL input is not a string
+test_that("ERROR when running scraper(): URL input should be a string", {
+  expect_error(scraper(url = 123, online = TRUE))
+  expect_error(scraper(url = TRUE, online = TRUE))
+})
+
+# Test scraper() to raise error when required URL input is not a valid Craiglist housing URL
+test_that("ERROR when running scraper(): URL input needs to be a valid Craiglist housing URL", {
+  expect_error(scraper(url = "https://www.haha.com", online = TRUE))
+  expect_error(scraper(url = "https://wiki.ubc.ca/Main_Page", online = TRUE))
+})
+
+#  Test scraper() to raise error the optional input `online` is not a Boolean
+test_that("ERROR when running scraper(): `online` need to be a Boolean", {
+  expect_error(scraper(url = url, online = 1))
+  expect_error(scraper(url = url, online = 'sunny'))
+  expect_error(scraper(url = url, online = '25yrs?'))
+})
+s
+# Tests on output
+data <- scraper(url = url, online = TRUE)
+
+# Test to confirm the output tibble is not empty
+test_that("Output tibble is not empty", {
+  expect_true(dim(data)[1] != 0)
+})
+
+# Test to confirm the dimension of the output tibble is correct
+test_that("Output tibble has the correct shape", {
+  expect_true(dim(data) == c(120,5))
+})
+
+# Test to confirm the data type of each column of the output tibble is string
+
+# 4 more tests
+
