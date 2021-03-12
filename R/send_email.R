41c9c116a9f02326bc17882d1b364077536b63bc
Ela Bandari <elabandari@gmail.com>
2021-03-12 00:26:48 -0800

    Minor fixes to examples and descriptions

--- R/send_email.R
+++ R/send_email.R
@@ -1,45 +1,44 @@
-library(tidyverse)
-library(mailR)
-
 #' A function to email search results.
 #'
-#' @param email_recipient A string containing the email address of the recipient.
+#' @param email_recipient A character vector containing the recipient's email.
 #' 
 #' @param filtered_data A tibble containing the filtered search results.
 #' 
-#' @param email_subject A string containing the desired subject of the email. 
+#' @param email_subject A character vector of desired subject of the email. 
 #' Optional argument with default value of  'Results from RHouseHunter'.
 #'
 #' @return None.
 #' @export
 #'
 #' @examples 
-#' send_email("helloworld@gmail.com", df)
-#' send_email("helloworld@gmail.com", df, "Scraping Results 2")
+#' send_email("elabandari@gmail.com", (tibble::tibble(name = c("house1"))))
+
 send_email <- function(email_recipient, filtered_data,
                        email_subject = 'Results from RHouseHunter') {
-  
+  # Pattern for validating email address
   email_pattern <- "\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>"
-  
-  if (!is.str(email_recipient)){
+  # Programming defensively 
+  if (!is.character(email_recipient)){
     rlang::abort("The email address must be a string. Aborting.")
     }
   if (grepl(email_pattern, email_recipient, ignore.case=TRUE) == FALSE) {
-  rlang::abort("You have input an invalid Email Address. Aborting.")
+    rlang::abort("You have input an invalid Email Address. Aborting.")
     } 
-  if (!tidyverse::is_tibble(filtered_data)) {
+  if (!is.character(email_subject)){
+    rlang::abort("The email subject must be a string. Aborting.")
+  }
+  if (!tibble::is_tibble(filtered_data)) {
     rlang::abort("The filtered results must be a tibble. Aborting.")
-    }
-  if (!tidyverse::is_tibble(filtered_data)) {
-  rlang::abort("The filtered results must be a tibble. Aborting.")
     }
   if (dim(filtered_data)[1] == 0) {
     rlang::abort("Your tibble is empty and no results to be emailed. Aborting.")
   }
-    
+  
+  # Writing tibble to tempfile as a csv  
   file_path <- tempfile()
   readr::write_csv(filtered_data, file_path)
   
+  # Emailing user the results
   mailR::send.mail(from = "pyhousehunter@gmail.com",
                    to = email_recipient,
                    subject = email_subject,
@@ -48,8 +47,8 @@ send_email <- function(email_recipient, filtered_data,
                                user.name = "pyhousehunter@gmail.com",            
                                passwd = "dsci524group6", tls = TRUE), 
                    authenticate = TRUE,
-                   send = TRUE,
                    attach.files = file_path,
-                   file.names = "Results.csv") 
+                   file.names = "Results.csv",
+                   send = TRUE) 
   }
 
