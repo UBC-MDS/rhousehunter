c0fa0ae0175094700f7ec13ebef0d45bf35836a0
JuntingHe <junting.heee@gmail.com>
2021-03-11 21:11:51 -0800

    Modify test-data_cleaner.R

--- tests/testthat/test-data_cleaner.R
+++ tests/testthat/test-data_cleaner.R
@@ -1,10 +1,5 @@
 # Tests on input
 
-testthat::test_that("Error in scraper(): URL input is missing", {
-  expect_error(scraper(online = TRUE))
-  expect_error(scraper())
-})
-
 ## Test to confirm that ValueError is raised when the required scraped_df input is not a tibble
 testthat::test_that("Error in data_cleaner(): the input is not a tibble",{
   testthat::expect_error(data_cleaner("raw.html"))
@@ -16,12 +11,12 @@ testthat::test_that("Error in data_cleaner(): the input tibble is empty",{
   testthat::expect_error(data_cleaner(tibble::tibble("price" = interger(), "house_type":character(), "listing_url": character())))
 })
 
-## Test to confirm that ValueError is raised when the scraped_df input is missing the price column
+## Test to confirm that error is raised when the scraped_df input is missing the price column
 testthat::test_that("Error in data_cleaner(): the input dataframe is missing the price column",{
   testthat::expect_error(data_cleaner(tibble::tibble("house_type"= c("1", "2"), "listing_url" = c("2", "3"))))
 })
 
-## Test to confirm that ValueError is raised when the scraped_df input is missing the house_type column
+## Test to confirm that error is raised when the scraped_df input is missing the house_type column
 testthat::test_that("Error in data_cleaner(): the input dataframe is missing the price column",{
   testthat::expect_error(data_cleaner(tibble::tibble("price"= c(1, 2), "listing_url" = c("2", "3"))))
 })
@@ -34,7 +29,7 @@ testthat::test_that("Error in data_cleaner(): the input dataframe is missing the
 
 # Tests on output
 
-toy_scraped_df <- readr::read.csv('tests/toy.csv')
+toy_scraped_df <- readr::read_csv('toy.csv')
 output_df <- data_cleaner(toy_scraped_df)
 toy_cleaned_df <- readr::read_csv("cleaned_toy.csv", col_types = readr::cols(price = "i", num_bedroom = "i", area_sqft = "i"))
 
@@ -44,9 +39,8 @@ testthat::test_that("The cleaned tibble should not be empty", {
 })
 
 ## Test to confirm the dimension of the output tibble is correct
-testthat::test_that("The cleaned tibble should have the same shape as the input tibble", {
+testthat::test_that("The cleaned tibble should have the same rows as the input tibble", {
   testthat::expect_true(dim(output_df)[1] == dim(toy_scraped_df)[1])
-  testthat::expect_true(dim(output_df)[2] == dim(toy_scraped_df)[1])
 })
 
 ## Test to confirm that the data type of price column of the output tibble is integer
@@ -71,10 +65,10 @@ testthat::test_that("The data type of price column of the output tibble should b
 
 ## Test to confirm that the output tibble have correct names ready for future filtering
 testthat::test_that("The columns of the output tibble should have correct names ready for future filtering", {
-  testthat::expect_equal(colnames(data), c("listing_url", "price", "num_bedroom", "area_sqft", "city"))
+  testthat::expect_equal(colnames(output_df), c("listing_url", "price", "num_bedroom", "area_sqft", "city"))
 })
 
 ## Test to confirm that the output tibble matches cleaned toy data
 testthat::test_that("The scraped data frame contains data in toy dataset", {
-  testthat::expect_true(dplyr::all_equal(output, toy_cleaned_df))
+  testthat::expect_true(dplyr::all_equal(output_df, toy_cleaned_df))
 })
